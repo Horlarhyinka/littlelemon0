8a0bfc6d331a87ad0735193f7c3aacecdfb08d5d
Horlarhyinka <danijufarouq2003@gmail.com>
2023-08-31 18:39:55 +0100

    completed project little lemon(my first django project)

---
+++ restaurant/views.py
@@ -0,0 +1,38 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import BookingForm
+from .models import Menu
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+def menu(request):
+    menu_data = Menu.objects.all()
+    maindata = {"menus":menu_data}
+    template_name = "menu.html"
+    for i in menu_data:
+        print(i.menu_item_description)
+    return render(request, template_name, maindata) 
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, "menu_item.html", {"menu_item": menu_item})
