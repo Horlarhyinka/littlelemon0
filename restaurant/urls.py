5f44ba34d7dd099871cdb8f5aa87e4d4c85632b6
Horlarhyinka <danijufarouq2003@gmail.com>
2024-10-04 03:22:53 +0100

    added wake server

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('check/', views.check, name="check server"),
     path('book/', views.book, name="book"),
     path('track/', views.track, name="track reservation"),
     path('menu/', views.menu, name="menu"),
