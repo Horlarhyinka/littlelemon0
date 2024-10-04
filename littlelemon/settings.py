5cb06b5f122e9597119d0b34ba6a2cb9e3ec8206
Horlarhyinka <danijufarouq2003@gmail.com>
2024-10-04 01:40:08 +0100

    updated settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     # "/static"
 ]
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
